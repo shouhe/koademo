9a7916bf4a3f1dc611bd6587bd3135be659b4710
shouhe <shouhe@meili-inc.com>
2017-02-24 15:29:03 +0800

    shouhe:up

--- model/User.js
+++ model/User.js
@@ -1,1 +1,12 @@
-export default User = {}
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize('blog', 'root', 'admin');
+export default class  User {
+  
+} 
+
+
+const User =  sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var Koa = require('koa');
 var app = new Koa();
 var logger = require('koa-logger')
+var log4js = require('log4js'); 
 import router from '../router'
 app.use(logger())
 app
